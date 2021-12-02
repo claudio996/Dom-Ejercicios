db2c93113eee4effada2c96cc5f4303b9c9ec9ad
claudio996 <claudioantonio.retamal@gmail.com>
2021-12-02 20:47:20 -0300

    Fronted form

---
+++ js/validation.js
@@ -0,0 +1,10 @@
+/*Este ejercicio contiste en detectar la camara web.
+ */
+const d = document,
+    n = navigator;
+
+
+export default function validation() { //selectores.
+    const $video = d.getElementById();
+
+}
